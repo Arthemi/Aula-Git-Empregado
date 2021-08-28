2a7b32dd315e671a5fd7b283893ac3f779c9b125
‘Lucas’ <lucasomacedo@gmail.com>
2021-08-28 13:33:53 -0300

    Commit inicial. Classe Empregado criado

--- Empregado.java
+++ Empregado.java
@@ -1,17 +1,20 @@
 public class Empregado{
-
     private String nome;
     private int idade;
+
     public String getNome (){
         return this.nome;
-}
+    }
+
     public void setNome (String nome){
         this.nome = nome;
-}
+    }
+
     public int getIdade (){
-    return this.idade;
-}
+        return this.idade;
+    }
+
     public void setIdade (int idade){
         this.idade = idade;
-}
+    }
 }
